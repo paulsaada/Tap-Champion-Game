847cdd30d90e210b882bb76e0dc8fec054f161d2
Paul Saada <paulsaada6@gmail.com>
2019-04-11 18:05:41 -0400

    styled game, added keyboard functionality

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-var playerOne = document.querySelector("#p1");
-var playerTwo = document.querySelector("#p2");
+// global vars
 var reset = document.querySelector("#reset");
 var scoreHeader = document.querySelector("h1");
 var p1Score = 0;
@@ -8,9 +7,10 @@ var winningScore = 5;
 var playerOneDisplay = document.querySelector("#PlayerOne");
 var playerTwoDisplay = document.querySelector("#PlayerTwo");
 var inputValue = document.querySelector("input");
-var p = document.querySelector("p span");
+var p = document.querySelector(".goal-score p span");
 var gameOver = false;
 
+// reset function
 function resetGame() {
 	gameOver = false;
 	p1Score = 0;
@@ -21,34 +21,49 @@ function resetGame() {
     playerTwoDisplay.classList.remove("winner");
 }
 
+// reset button
 reset.addEventListener("click", function(){
 	resetGame();
 });
 
+// r to reset
+document.addEventListener("keyup", function(){
+	if (event.key === 'r') {
+		resetGame();
+	}
+});
+
+// set winning score via user input
 inputValue.addEventListener("change", function(){
 	p.textContent = inputValue.value;
 	winningScore = Number(inputValue.value);
 	resetGame();
 });
 
-playerOne.addEventListener("click", function(){
-	if (!gameOver) {
-		p1Score++;
-		if (p1Score === winningScore) {
-			playerOneDisplay.classList.add("winner");
-			gameOver = true;
+// player one
+document.addEventListener("keyup", function(){
+	if (event.key === 'z') {
+		if (!gameOver) {
+			p1Score++;
+			if (p1Score === winningScore) {
+				playerOneDisplay.classList.add("winner");
+				gameOver = true;
+			}
+			playerOneDisplay.textContent = p1Score;
 		}
-		playerOneDisplay.textContent = p1Score;
-	} 
+	}
 });
 
-playerTwo.addEventListener("click", function(){
-	if (!gameOver) {
-		p2Score++;
-		if (p2Score === winningScore) {
-			playerTwoDisplay.classList.add("winner");
-			gameOver = true;
+// player two
+document.addEventListener("keyup", function(){
+	if (event.key === 'm') {
+		if (!gameOver) {
+			p2Score++;
+			if (p2Score === winningScore) {
+				playerTwoDisplay.classList.add("winner");
+				gameOver = true;
+			}
+			playerTwoDisplay.textContent = p2Score;
 		}
-		playerTwoDisplay.textContent = p2Score;
-	} 
+	}
 });
